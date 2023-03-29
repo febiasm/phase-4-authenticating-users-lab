047d689784a0b00609319e4d6c5992a50e8dfd60
febias <73440637+febiasm@users.noreply.github.com>
2023-03-29 20:52:51 +0300

    Create users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+    def show
+      user = User.find_by(id: session[:user_id])
+      if user
+        render json: user
+      else
+        render json: { error: 'Not logged in' }, status: :unprocessable_entity
+      end
+    end
+  end
+  
